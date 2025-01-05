c96d34d58c1d13bf81840e1746b0d36a88259276
swasty jay <amekpoagbedaniel@gmail.com>
2025-01-05 14:23:48 +0000

    fetching questions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import Ready from "./Ready";
+
+const initialState = {
+  quetions: [],
+  status: "Loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, quetions: action.payload, status: "ready" };
+
+    case "datafailed":
+      return { ...state, status: "Error" };
+    default:
+      throw new Error("action unknown");
+  }
+}
 
 export default function App() {
+  const [{ quetions, status }, dispatch] = useReducer(reducer, initialState);
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json)
-      .then((data) => console.log(data))
-      .catch((err) => console.error("err"));
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "datafailed" }));
   }, []);
 
   return (
@@ -15,8 +35,9 @@ export default function App() {
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Questions?</p>
+        {status === "Loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <Ready />}
       </Main>
     </div>
   );
---
+++ src/Ready.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Ready() {
+  return (
+    <div className="start">
+      <h2>welcome to The React Quiz</h2>
+      <h3>X questions to test your React knowledge</h3>
+      <button className="btn">let's start</button>
+    </div>
+  );
+}
+
+export default Ready;
