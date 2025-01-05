ab9136ae20c6b8f239b7d500f2cb734468caf0ba
swasty jay <amekpoagbedaniel@gmail.com>
2025-01-05 11:21:17 +0000

    fetching questions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
-import DateCounter from "./DateCounter";
+import Main from "./Main";
 
 export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json)
+      .then((data) => console.log(data))
+      .catch((err) => console.error("err"));
+  }, []);
+
   return (
     <div>
       <Header />
-      <DateCounter />
+
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main({ children }) {
+  return (
+    <div>
+      <main className="main">{children}</main>
+    </div>
+  );
+}
+
+export default Main;
